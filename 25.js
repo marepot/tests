ca2e7c5de713905d0429e3bfe237ee88bbee1562
marepot <86474207+marepot@users.noreply.github.com>
2022-01-06 20:01:06 +0300

    Add files via upload

---
+++ 25.js
@@ -0,0 +1,25 @@
+;(function (exports) {
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ]
+  exports.name = function (number) {
+    return monthNames[number]
+  }
+  exports.number = function (name) {
+    return monthNames.indexOf(name)
+  }
+})((month = {}))
+
+console.log(month.name(2))
+console.log(month.number("November"))
